4d045ede6508ae9e1852fee8c7bb24732b132844
Guddukp <guddukp397@gmail.com>
2021-02-01 15:22:10 +0530

    deploy change

--- myweb/myproject/myproject/settings.py
+++ myweb/myproject/myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'myn*d_wzzap7%6k$j)2_x+_1!2vpfnp=xf_nu3oozkjo9#!aw7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,6 +136,5 @@ MEDIA_URL = '/media/'
     os.path.join(BASE_DIR, 'media')
 ]"""
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
- 
 
- 
+
--- myweb/myproject/myproject/urls.py
+++ myweb/myproject/myproject/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -11,9 +13,11 @@ urlpatterns = [
     url(r'' , include('subcat.urls')),
     url(r'' , include('contactform.urls')),
     url(r'' , include('trending.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-    
+
